1aca0a32304bfceaedc4c9ff4cd4d6f580faf27b
qvaseel <vasilina.makarkina@gmail.com>
2026-09-21 03:30:26 +0700

    some fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -10,6 +10,9 @@ export class PrismaService
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
 
     super({ adapter });
